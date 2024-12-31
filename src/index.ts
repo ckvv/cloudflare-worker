3ef200c4dd9fcb9c51fa5292b5e74733f0391b85
ckvv <k_0520@qq.com>
2024-12-31 15:29:22 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -24,21 +24,23 @@ app.get('/', async(c) => {
     delimiter,
     startAfter,
   });
-  return c.json(result.objects);
+  return c.json(result.objects.map(object => {
+    
+  }));
 })
 
-app.post('/:key', async(c) => {
+app.post('/:key{.+}', async(c) => {
   const body = await c.req.parseBody()
   await c.env.MY_BUCKET.put(c.req.param('key'), body['file']);
   return c.json({});
 })
 
-app.delete('/:key', async(c) => {
+app.delete('/:key{.+}', async(c) => {
   await c.env.MY_BUCKET.delete(c.req.param('key'));
   return c.json({});
 })
 
-app.get('/:key', async (c) => {
+app.get('/:key{.+}', async (c) => {
   const result = await c.env.MY_BUCKET.get(c.req.param('key'))
   return c.body(result?.body);
 })
