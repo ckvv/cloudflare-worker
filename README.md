76ed40fdc70df49198beaf6509b3a73a67490bcc
ckvv <k_0520@qq.com>
2024-12-31 14:40:07 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# sugrec
+ cloudflare wrangler
---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import type { R2Bucket } from '@cloudflare/workers-types';
+import { Hono } from 'hono'
+import { cors } from 'hono/cors';
+
+type Bindings = {
+  MY_BUCKET: R2Bucket
+}
+
+const app = new Hono<{Bindings: Bindings}>()
+
+app.use(
+  '*',
+  cors({
+    origin: (origin, c) => origin,
+  })
+)
+
+app.get('/', async(c) => {
+  const { limit, prefix, cursor, delimiter, startAfter} = c.req.query()
+  const result = await c.env.MY_BUCKET.list({
+    limit: limit ? parseInt(limit) : undefined,
+    prefix,
+    cursor,
+    delimiter,
+    startAfter,
+  });
+  return c.json(result.objects);
+})
+
+app.post('/:key', async(c) => {
+  const body = await c.req.parseBody()
+  await c.env.MY_BUCKET.put(c.req.param('key'), body['file']);
+  return c.json({});
+})
+
+app.delete('/:key', async(c) => {
+  await c.env.MY_BUCKET.delete(c.req.param('key'));
+  return c.json({});
+})
+
+app.get('/:key', async (c) => {
+  const result = await c.env.MY_BUCKET.get(c.req.param('key'))
+  return c.body(result?.body);
+})
+
+export default app
