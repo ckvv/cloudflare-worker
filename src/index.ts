a67f59ae309635271e370488fc7700d3ef1401ec
ckvv <k_0520@qq.com>
2024-12-31 16:10:15 +0800

    save

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,61 @@
-import { type R2Bucket } from '@cloudflare/workers-types';
+import type { R2Bucket } from '@cloudflare/workers-types'
 import { Hono } from 'hono'
-import { cors } from 'hono/cors';
+import { cors } from 'hono/cors'
 
-type Bindings = {
+interface Bindings {
   MY_BUCKET: R2Bucket
 }
 
-const app = new Hono<{Bindings: Bindings}>()
+const app = new Hono<{ Bindings: Bindings }>()
 
 app.use(
   '*',
   cors({
-    origin: (origin, c) => origin,
-  })
+    origin: (origin, _c) => origin,
+  }),
 )
 
-app.get('/', async(c) => {
-  const { limit, prefix, cursor, delimiter, startAfter} = c.req.query()
+app.get('/', async (c) => {
+  const { limit, prefix, cursor, delimiter, startAfter } = c.req.query()
   const result = await c.env.MY_BUCKET.list({
-    limit: limit ? parseInt(limit) : undefined,
+    limit: limit ? Number.parseInt(limit) : undefined,
     prefix,
     cursor,
     delimiter,
     startAfter,
-  });
+  })
   return c.json(result.objects.map(object => ({
     key: object.key,
     size: object.size,
     uploaded: object.uploaded,
-  })));
+  })))
 })
 
-app.post('/:key{.+}', async(c) => {
+app.post('/:key{.+}', async (c) => {
   const body = await c.req.parseBody()
-  await c.env.MY_BUCKET.put(c.req.param('key'), body['file'] as any, {
-    httpMetadata: new Headers({
-      'name': 'ck',
-    }) as any,
+  const file = body.file as File
+  await c.env.MY_BUCKET.put(c.req.param('key'), file as any, {
     customMetadata: {
-      'age': '18',
+      filename: encodeURIComponent(file.name),
     },
-  });
-  return c.json({});
+  })
+  return c.json({})
 })
 
-app.delete('/:key{.+}', async(c) => {
-  await c.env.MY_BUCKET.delete(c.req.param('key'));
-  return c.json({});
+app.delete('/:key{.+}', async (c) => {
+  await c.env.MY_BUCKET.delete(c.req.param('key'))
+  return c.json({})
 })
 
 app.get('/:key{.+}', async (c) => {
   const result = await c.env.MY_BUCKET.get(c.req.param('key'))
-  return c.body(result?.body as any);
+  const type = c.req.query('type')
+
+  const filename = result?.customMetadata?.filename
+  if (filename) {
+    c.header('Content-Disposition', `${type || 'inline'}; filename="${encodeURIComponent(filename)}"`)
+  }
+  return c.body(result?.body as any)
 })
 
 export default app
