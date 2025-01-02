2bd5e1f75946765a0ee86d5bda6867695b91f586
ckvv <k_0520@qq.com>
2025-01-02 13:56:26 +0800

    参数验证

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import { type R2Bucket } from '@cloudflare/workers-types'
 import { Hono } from 'hono'
+import { z } from 'zod'
 import { cors } from 'hono/cors'
+
+const SCHEMA = {
+  R2ListOptions: z.object({
+    limit: z.coerce.number().int().gt(0).lte(1000).optional(),
+    prefix: z.string().optional(),
+    cursor: z.string().optional(),
+    delimiter: z.string().optional(),
+    startAfter: z.string().optional(),
+  }),
+  R2PutOptions: z.object({
+    storageClass: z.enum(['Standard', 'InfrequentAccess']).optional(),
+  }),
+  R2GetOptions: z.any(),
+}
 
 interface Bindings {
   MY_BUCKET: R2Bucket
@@ -8,33 +23,36 @@ interface Bindings {
 
 const app = new Hono<{ Bindings: Bindings }>()
 
+app.onError((error, c) => {
+  return c.json({
+    success: false,
+    error,
+  })
+})
+
 app.use(
   '*',
   cors({
-    origin: (origin, _c) => origin,
+    origin: (origin, _c) => origin || '*',
   }),
 )
 
 app.get('/', async (c) => {
-  const { limit, prefix, cursor, delimiter, startAfter } = c.req.query()
-  const result = await c.env.MY_BUCKET.list({
-    limit: limit ? Number.parseInt(limit) : undefined,
-    prefix,
-    cursor,
-    delimiter,
-    startAfter,
-  })
-  return c.json(result.objects.map(object => ({
-    key: object.key,
-    size: object.size,
-    uploaded: object.uploaded,
-  })))
+  const result = await c.env.MY_BUCKET.list(SCHEMA.R2ListOptions.parse(c.req.query()))
+  return c.json(result.objects)
+  // return c.json(result.objects.map(object => ({
+  //   key: object.key,
+  //   size: object.size,
+  //   uploaded: object.uploaded,
+  // })))
 })
 
 app.post('/:key{.+}', async (c) => {
   const body = await c.req.parseBody()
   const file = body.file as File
+  const options = SCHEMA.R2PutOptions.parse(body.options);
   await c.env.MY_BUCKET.put(c.req.param('key'), file as any, {
+    storageClass: options.storageClass,
     customMetadata: {
       filename: encodeURIComponent(file.name),
     },
@@ -48,14 +66,13 @@ app.delete('/:key{.+}', async (c) => {
 })
 
 app.get('/:key{.+}', async (c) => {
-  const result = await c.env.MY_BUCKET.get(c.req.param('key'))
-  const type = c.req.query('type')
+  const result: any = await c.env.MY_BUCKET.get(c.req.param('key'), SCHEMA.R2GetOptions.parse(c.req.query()))
 
   const filename = result?.customMetadata?.filename
   if (filename) {
-    c.header('Content-Disposition', `${type || 'inline'}; filename="${encodeURIComponent(filename)}"`)
+    c.header('Content-Disposition', `inline; filename="${encodeURIComponent(filename)}"`)
   }
-  return c.body(result?.body as any)
+  return c.body(result?.body)
 })
 
 export default app
