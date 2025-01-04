f27cb835e013ae6910eaf92001edb3977f5e7bb8
ckvv <k_0520@qq.com>
2025-01-04 21:45:04 +0800

    style: eslint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import type { R2Bucket, R2GetOptions, R2Range } from '@cloudflare/workers-types'
+import type { R2Bucket } from '@cloudflare/workers-types'
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { z } from 'zod'
@@ -14,7 +14,6 @@ const SCHEMA = {
   R2PutOptions: z.object({
     storageClass: z.enum(['Standard', 'InfrequentAccess']).optional(),
   }).optional(),
-  R2GetOptions: z.any().optional(),
 }
 
 interface Bindings {
@@ -25,7 +24,7 @@ interface Bindings {
 const app = new Hono<{ Bindings: Bindings }>()
 
 app.onError((error, c) => {
-  console.log(error);
+  console.log(error)
   return c.json({
     success: false,
     error,
@@ -50,9 +49,9 @@ app.get('/', async (c) => {
 
 app.post('/:key{.+}', async (c) => {
   const body = await c.req.parseBody()
-  const file = body.file as File;
-  const key = c.req.param('key');
-  const options = SCHEMA.R2PutOptions.parse(body.options);
+  const file = body.file as File
+  const key = c.req.param('key')
+  const options = SCHEMA.R2PutOptions.parse(body.options)
 
   await c.env.MY_BUCKET.put(key, file as any, {
     storageClass: options?.storageClass,
@@ -64,39 +63,40 @@ app.post('/:key{.+}', async (c) => {
   return c.json({
     size: file.size,
     key,
-  });
+  })
 })
 
 app.delete('/:key{.+}', async (c) => {
-  const auth = c.req.header('Authorization');
-  if(c.env.password && auth !== c.env.password) {
-    throw new Error('password error');
+  const auth = c.req.header('Authorization')
+  if (c.env.password && auth !== c.env.password) {
+    throw new Error('password error')
   }
   await c.env.MY_BUCKET.delete(c.req.param('key'))
   return c.json({})
 })
 
 app.get('/:key{.+}', async (c) => {
-  const options: { range?: { offset?: number, length?: number, suffix?: number } } = {};
-  const range = c.req.header('range') || '';
+  const options: { range?: { offset?: number, length?: number, suffix?: number } } = {}
+  const range = c.req.header('range') || ''
 
-  if(range) {
-    const [start, end] = range?.slice(6).split('-').map(v => v ? parseInt(v) : undefined);
-    if(start !== undefined) {
-      options.range =  {
-        offset: start,
-        length: end ?  end - start : undefined,
-      }
-    } else if (end !== undefined) {
+  if (range) {
+    const [start, end] = range?.slice(6).split('-').map(v => v ? Number.parseInt(v) : undefined)
+    if (start !== undefined) {
       options.range = {
-        suffix: end
+        offset: start,
+        length: end ? end - start : undefined,
+      }
+    }
+    else if (end !== undefined) {
+      options.range = {
+        suffix: end,
       }
     }
   }
   const result = await c.env.MY_BUCKET.get(c.req.param('key'), options as any)
 
-  if(!result) {
-    return c.body( null, 404)
+  if (!result) {
+    return c.body(null, 404)
   }
   const filename = result?.customMetadata?.filename
   if (filename) {
@@ -107,18 +107,19 @@ app.get('/:key{.+}', async (c) => {
   c.header('Content-Type', result.customMetadata?.type)
   c.header('Accept-Ranges', 'bytes')
 
-  if(options.range) {
-    const rangeStart = options.range?.offset ?? 0;
-    const rangeEnd = options.range.length ? rangeStart + options.range.length - 1 : result.size - 1;
-    c.header('Content-Range', `bytes ${rangeStart}-${rangeEnd}/${result.size}`);
-    c.header('Content-Length', `${rangeEnd - rangeStart + 1}`);
+  if (options.range) {
+    const rangeStart = options.range?.offset ?? 0
+    const rangeEnd = options.range.length ? rangeStart + options.range.length - 1 : result.size - 1
+    c.header('Content-Range', `bytes ${rangeStart}-${rangeEnd}/${result.size}`)
+    c.header('Content-Length', `${rangeEnd - rangeStart + 1}`)
 
     c.status(206)
-  } else {
-    c.status(200)
-    c.header('Content-Length', `${result.size}`);
   }
-  
+  else {
+    c.status(200)
+    c.header('Content-Length', `${result.size}`)
+  }
+
   return c.body((result as any).body)
 })
 
