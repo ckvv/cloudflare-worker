bafe3336394d1b366b42d2ad53e511d83ee5bf5d
ckvv <k_0520@qq.com>
2025-01-08 14:11:59 +0800

    feat: 添加 demo

--- src/api/geopattern.ts
+++ src/api/geopattern.ts
@@ -16,4 +16,57 @@ app.get('/:filename{.+\\.svg}', async (c) => {
   return c.body(GeoPattern.generate(c.req.param('filename').slice(0, -4), options).svg)
 })
 
+app.get('/', async (c) => {
+  return c.html(`<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>@ckpack/geopattern</title>
+</head>
+<style>
+  html,
+  body {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+  #geo-pattern {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  #geo-pattern-input {
+    padding: .5em 1em;
+  }
+</style>
+
+<body>
+  <div id="geo-pattern">
+    <input id="geo-pattern-input" onchange="handlerChange" />
+  </div>
+</body>
+<script type="module">
+  import { GeoPattern } from "https://cdn.jsdelivr.net/npm/@ckpack/geopattern@0.0.2/dist/index.min.js";
+
+  const BG = document.querySelector('#geo-pattern')
+  function makePattern(value = '') {
+    BG.style.backgroundImage = GeoPattern.generate(value, {
+      color: '#3231aa',
+    }).toDataUrl()
+  }
+
+  document.querySelector('#geo-pattern-input').addEventListener('input', (e) => {
+    makePattern(e.target.value)
+  })
+  makePattern()
+</script>
+
+</html>`)
+})
+
 export default app
